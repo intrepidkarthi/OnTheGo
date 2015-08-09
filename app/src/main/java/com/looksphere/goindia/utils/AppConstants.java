d99b9ca48b23cb6de5aac9d84dddc02975ab70ed
Karthikeyan NG <karthikeyan.ng@ymedialabs.com>
2015-08-09 12:45:06 +0530

    Removing unncessary contants

--- app/src/main/java/com/looksphere/goindia/utils/AppConstants.java
+++ app/src/main/java/com/looksphere/goindia/utils/AppConstants.java
@@ -4,22 +4,5 @@ package com.looksphere.goindia.utils;
  * Created by karthi on 30/03/14.
  */
 public class AppConstants {
-    public static final String SERVER_BASE_URL = "http://yourwebsitehere";
-    public static final String GRAPH_API_URL = "https://graph.facebook.com/";
-    public static final String FETCH_NEAREST_DIRT="/fetchnearestdirt?location=";
-    public static final String GRAPH_API_IMAGE_SUFFIX = "/picture?type=small&width=200&height=200";
-    public static final String SERVER_HOST_ADDRESS="http://www.swachh.org";
-    public static final String SIGNUP = "/signup";
-    public static final String ADD_NEW_POST="/protected/savedirt";
-    public static final String FETCH_COMMENTS_BYID="/fetchcommentsbydirt?dirtId=";
-    public static final String LIKE_POST="/protected/addlike";
-    public static final String FETCH_USER_PROFILE="/fetchuserprofile";
-    public static final String COMMENT_ON_POST="/protected/addcomments";
-    public static final String ASSIGNING_TASK="/protected/assigndirt";
-    public static final String FETCH_ALL_ASSIGNED="/protected/fetchassigneddirt?location=";
-    public static final String COMPLETE_A_TASK="/protected/completedirt";
-    public static final String FETCH_ALL_COMPLETED="/protected/fetchcompleteddirt";
-
-    public static final String CLOUDINARY_IMAGEURL_PREFIX = "";
-    public static final String CLOUDINARY_IMAGEURL_SUFFIX = "";
+ //Add your server details here
 }
